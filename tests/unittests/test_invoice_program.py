393eeaeb5b081a6a258f0ff22a45b8c940a7218e
Simone Campagna <simone.campagna@tiscali.it>
2015-01-26 22:25:40 +0100

    re-added validate tests

--- tests/unittests/test_invoice_program.py
+++ tests/unittests/test_invoice_program.py
@@ -27,8 +27,10 @@ import tempfile
 import unittest
 
 from invoice.log import get_null_logger
-from invoice.error import InvoiceDuplicatedNumberError
+from invoice.error import InvoiceDuplicatedNumberError, InvoiceWrongNumberError
 from invoice.invoice_program import InvoiceProgram
+from invoice.invoice_collection import InvoiceCollection
+from invoice.invoice import Invoice
 from invoice.database.db_types import Path
 
 class Print(object):
@@ -127,6 +129,53 @@ anno                       2014
     def setUp(self):
         self.dirname = Path.db_to(os.path.join(os.path.dirname(__file__), '..', '..', 'example'))
         self.logger = get_null_logger()
+        self._invoice_001_peter_parker = Invoice(
+            doc_filename='2015_001_peter_parker.doc',
+            year=2015, number=1,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 1),
+            income=200.0, currency='euro')
+        self._invoice_002_peter_parker = Invoice(
+            doc_filename='2015_002_peter_parker.doc',
+            year=2015, number=2,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 2),
+            income=100.0, currency='euro')
+        self._invoice_003_peter_parker = Invoice(
+            doc_filename='2015_003_peter_parser.doc',
+            year=2015, number=3,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 3),
+            income=150.0, currency='euro')
+        self._invoices = [
+            self._invoice_001_peter_parker,
+            self._invoice_002_peter_parker,
+            self._invoice_003_peter_parker,
+        ]
+        self._invoice_004_parker_peter = Invoice(
+            doc_filename='2015_004_parker_peter.doc',
+            year=2015, number=4,
+            name='Parker B. Peter', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 4),
+            income=200.0, currency='euro')
+        self._invoice_004_peter_parker_wrong_date = Invoice(
+            doc_filename='2015_004_peter_parker.doc',
+            year=2015, number=4,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 2),
+            income=200.0, currency='euro')
+        self._invoice_004_peter_parker_wrong_number = Invoice(
+            doc_filename='2015_004_peter_parker.doc',
+            year=2015, number=6,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 5),
+            income=200.0, currency='euro')
+        self._invoice_004_peter_parker_duplicated_number = Invoice(
+            doc_filename='2015_004_peter_parker.doc',
+            year=2015, number=self._invoices[-1].number,
+            name='Peter B. Parker', tax_code='PRKPRT01A01B123C', 
+            city='New York', date=datetime.date(2015, 1, 5),
+            income=200.0, currency='euro')
 
     # invoice
     def test_InvoiceProgram(self):
@@ -205,4 +254,93 @@ KNTCRK01G01H663Y 2014      5
                     remove_orphaned=None,
                 )
 
+    def test_InvoiceProgram_validate_ok(self):
+        with tempfile.NamedTemporaryFile() as db_filename:
+            p = Print()
+            invoice_program = InvoiceProgram(
+                db_filename=db_filename.name,
+                logger=self.logger,
+                trace=False,
+                print_function=p,
+            )
+    
+            invoice_collection = InvoiceCollection(self._invoices, logger=self.logger)
+            validation_result = invoice_program.validate_invoice_collection(invoice_collection)
+            self.assertEqual(validation_result.num_errors(), 0)
+            self.assertEqual(validation_result.num_warnings(), 0)
+    
+    def test_InvoiceProgram_validate_warning_multiple_names(self):
+        with tempfile.NamedTemporaryFile() as db_filename:
+            p = Print()
+            invoice_program = InvoiceProgram(
+                db_filename=db_filename.name,
+                logger=self.logger,
+                trace=False,
+                print_function=p,
+            )
+    
+            invoice_collection = InvoiceCollection(self._invoices + [self._invoice_004_parker_peter], logger=self.logger)
+            validation_result = invoice_program.validate_invoice_collection(invoice_collection)
+            self.assertEqual(validation_result.num_errors(), 0)
+            self.assertEqual(validation_result.num_warnings(), 1)
+            for doc_filename, warnings in validation_result.warnings().items():
+                self.assertEqual(doc_filename, self._invoice_004_parker_peter.doc_filename)
+    
+    def test_InvoiceProgram_validate_error_wrong_date(self):
+        with tempfile.NamedTemporaryFile() as db_filename:
+            p = Print()
+            invoice_program = InvoiceProgram(
+                db_filename=db_filename.name,
+                logger=self.logger,
+                trace=False,
+                print_function=p,
+            )
+    
+            invoice_collection = InvoiceCollection(self._invoices + [self._invoice_004_peter_parker_wrong_date], logger=self.logger)
+            validation_result = invoice_program.validate_invoice_collection(invoice_collection)
+            self.assertEqual(validation_result.num_errors(), 1)
+            self.assertEqual(validation_result.num_warnings(), 0)
+            for doc_filename, errors in validation_result.errors().items():
+                self.assertEqual(doc_filename, self._invoice_004_peter_parker_wrong_date.doc_filename)
+    
+    def test_InvoiceProgram_validate_error_wrong_number(self):
+        with tempfile.NamedTemporaryFile() as db_filename:
+            p = Print()
+            invoice_program = InvoiceProgram(
+                db_filename=db_filename.name,
+                logger=self.logger,
+                trace=False,
+                print_function=p,
+            )
+    
+            invoice_collection = InvoiceCollection(self._invoices + [self._invoice_004_peter_parker_wrong_number], logger=self.logger)
+            validation_result = invoice_program.validate_invoice_collection(invoice_collection)
+            self.assertEqual(validation_result.num_errors(), 1)
+            self.assertEqual(validation_result.num_warnings(), 0)
+            for doc_filename, errors in validation_result.errors().items():
+                self.assertEqual(doc_filename, self._invoice_004_peter_parker_wrong_number.doc_filename)
+                self.assertEqual(len(errors), 1)
+                exc_type, message = errors[0]
+                self.assertIs(exc_type, InvoiceWrongNumberError)
+    
+    def test_InvoiceProgram_validate_error_duplicated_number(self):
+        with tempfile.NamedTemporaryFile() as db_filename:
+            p = Print()
+            invoice_program = InvoiceProgram(
+                db_filename=db_filename.name,
+                logger=self.logger,
+                trace=False,
+                print_function=p,
+            )
+    
+            invoice_collection = InvoiceCollection(self._invoices + [self._invoice_004_peter_parker_duplicated_number], logger=self.logger)
+            validation_result = invoice_program.validate_invoice_collection(invoice_collection)
+            self.assertEqual(validation_result.num_errors(), 1)
+            self.assertEqual(validation_result.num_warnings(), 0)
+            for doc_filename, errors in validation_result.errors().items():
+                self.assertEqual(doc_filename, self._invoice_004_peter_parker_duplicated_number.doc_filename)
+                self.assertEqual(len(errors), 1)
+                exc_type, message = errors[0]
+                self.assertIs(exc_type, InvoiceDuplicatedNumberError)
+        
 
