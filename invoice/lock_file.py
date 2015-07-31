9ff9d7d7362a62ce5bdeba585b7ab94f2f30be83
Simone Campagna <simone.campagna@tiscali.it>
2015-07-31 17:27:53 +0200

    daemon and lock_file indentation

--- invoice/lock_file.py
+++ invoice/lock_file.py
@@ -33,71 +33,64 @@ import io
 __author__ = "Simone Campagna"
 
 class LockFile(io.FileIO): # pragma: no cover
-  def __init__(self, name, mode='w'):
-    super().__init__(name, mode)
-
-  def acquire(self, blocking=None, timeout=None):
-    #sys.stderr.write("LockFile::ACQUIRE\n")
-    if blocking is None:
-      if timeout is None:
-        blocking = True
-      else:
-        blocking = False
-
-    lock_op = fcntl.LOCK_EX
-    if not blocking:
-      lock_op += fcntl.LOCK_NB
-    count = 0
-    interval = 0.1
-    if timeout is not None:
-      count = int(round(timeout/interval, 0))
-    if count <= 0:
-      count = 1
-    for i in range(count):
-      try:
-        #fcntl.fcntl(self.fileno(), lock_op, os.O_NDELAY)
-        fcntl.lockf(self.fileno(), lock_op)
-        return True
-      except IOError as e:
-        if e.errno in (errno.EACCES, errno.EAGAIN):
-          if timeout:
-            time.sleep(interval)
-          continue
-      except:
-        import traceback
-        traceback.print_exc()
-        time.sleep(interval)
-    return False
-
-  #def acquire_nb(self):
-  #  try:
-  #    fcntl.lockf(self, fcntl.LOCK_EX + fcntl.LOCK_NB)
-  #    return True
-  #  except IOError as e:
-  #    return False
-
-  def release(self):
-    fcntl.lockf(self, fcntl.LOCK_UN)
-    pass
-
-  def is_locked(self):
-    try:
-      fcntl.lockf(self.fileno(), fcntl.LOCK_EX + fcntl.LOCK_NB)
-      return False
-    except IOError as e:
-      if e.errno in (errno.EACCES, errno.EAGAIN):
-        return True
-
-  def close(self):
-      self.release()
-      super(LockFile, self).close()
-
-  def __del__(self):
-      try:
-          self.release()
-      except:
-          pass
-      try:
-          super(LockFile, self).__del__()
-      except:
-          pass
+    def __init__(self, name, mode='w'):
+      super().__init__(name, mode)
+  
+    def acquire(self, blocking=None, timeout=None):
+        #sys.stderr.write("LockFile::ACQUIRE\n")
+        if blocking is None:
+            if timeout is None:
+                blocking = True
+            else:
+                blocking = False
+    
+        lock_op = fcntl.LOCK_EX
+        if not blocking:
+            lock_op += fcntl.LOCK_NB
+        count = 0
+        interval = 0.1
+        if timeout is not None:
+            count = int(round(timeout/interval, 0))
+        if count <= 0:
+            count = 1
+        for i in range(count):
+            try:
+                #fcntl.fcntl(self.fileno(), lock_op, os.O_NDELAY)
+                fcntl.lockf(self.fileno(), lock_op)
+                return True
+            except IOError as e:
+                if e.errno in (errno.EACCES, errno.EAGAIN):
+                    if timeout:
+                        time.sleep(interval)
+                    continue
+            except:
+                import traceback
+                traceback.print_exc()
+                time.sleep(interval)
+        return False
+  
+    def release(self):
+        fcntl.lockf(self, fcntl.LOCK_UN)
+        pass
+  
+    def is_locked(self):
+        try:
+            fcntl.lockf(self.fileno(), fcntl.LOCK_EX + fcntl.LOCK_NB)
+            return False
+        except IOError as e:
+            if e.errno in (errno.EACCES, errno.EAGAIN):
+              return True
+  
+    def close(self):
+        self.release()
+        super(LockFile, self).close()
+  
+    def __del__(self):
+        try:
+            self.release()
+        except:
+            pass
+        try:
+            super(LockFile, self).__del__()
+        except:
+            pass
